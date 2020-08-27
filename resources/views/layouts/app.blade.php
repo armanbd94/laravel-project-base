d3fe2a280b40251883fea688c63cfc22b655dc9b
Mohammad Arman <arman.ctgbd1394@gmail.com>
2020-08-28 01:23:55 +0600

    All Files Added

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Meta tags -->
+  <base href="{{ asset('/') }}"/>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Wieldy - A fully responsive, HTML5 based admin template">
+  <meta name="keywords" content="Responsive, HTML5, admin theme, business, professional, jQuery, web design, CSS3, sass">
+  <!-- /meta tags -->
+  <title>Wieldy - Admin Dashboard</title>
+
+  <!-- Site favicon -->
+  <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
+  <!-- /site favicon -->
+
+  <link rel="stylesheet" href="css/app.css">
+
+</head>
+<body class="dt-sidebar--fixed dt-header--fixed">
+
+<!-- Loader -->
+<div class="dt-loader-container">
+  <div class="dt-loader">
+    <svg class="circular" viewBox="25 25 50 50">
+      <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"></circle>
+    </svg>
+  </div>
+</div>
+<!-- /loader -->
+
+<!-- Root -->
+<div class="dt-root">
+  <!-- Header -->
+@include('include.header')
+  <!-- /header -->
+
+  <!-- Site Main -->
+  <main class="dt-main">
+    <!-- Sidebar -->
+    <x-sidebar/>
+    <!-- /sidebar -->
+
+    <!-- Site Content Wrapper -->
+    <div class="dt-content-wrapper">
+
+      <!-- Site Content -->
+      @yield('content')
+      <!-- /site content -->
+
+      <!-- Footer -->
+      @include('include.footer')
+      <!-- /footer -->
+
+    </div>
+    <!-- /site content wrapper -->
+
+    <!-- Theme Chooser -->
+    <div class="dt-customizer-toggle">
+      <a href="javascript:void(0)" data-toggle="customizer"> <i class="icon icon-spin icon-setting"></i> </a>
+    </div>
+    <!-- /theme chooser -->
+
+    <!-- Customizer Sidebar -->
+    <x-right-sidebar/>
+    <!-- /customizer sidebar -->
+
+  </main>
+</div>
+<!-- /root -->
+
+<!-- Optional JavaScript -->
+<script src="js/app.js"></script>
+<script src="js/perfect-scrollbar.min.js"></script>
+<!-- /perfect scrollbar jQuery -->
+
+<script src="js/script.js"></script>
+<script src="js/custom/charts/dashboard-crypto.js"></script>
+</body>
+</html>
